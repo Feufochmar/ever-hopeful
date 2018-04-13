ad6e7f2760e059b5acceeae9997a4d5f9aba07c4
Feufochmar <feufochmar.gd@gmail.com>
2018-04-13 22:27:52 +0200

    Move the logic of uploading inside lib. Copy most of the model for pages as model for assets.

--- routes/upload.js
+++ routes/upload.js
@@ -1,12 +1,12 @@
 /* global Git */
 
 var app = require('../lib/app').getInstance()
+var assets = require('../lib/assets')
 var express = require('express')
-var fs = require('fs')
-var mkdirp = require('mkdirp')
 var multer = require('multer')
-var path = require('path')
 var router = express.Router()
+
+assets.use(Git)
 
 // File Uploads
 var uploadSubdir = app.locals.config.get('application').uploadSubdir
@@ -23,62 +23,18 @@ function _uploadFile(req, res, next) {
     return
   }
 
-  var repFileName = req.body.rep_file_name
-  var repFile = uploadDirectory + '/' + repFileName
-  var gitFile = uploadSubdir + '/' + repFileName
+  if (req.body.rep_file_name === '') {
+    res.render('upload_status', {message: 'Filename cannot be empty.'})
+    return
+  }
 
-  var tempFile = req.file.path
-  var outputMsg = ''
+  var asset = new assets.Asset(req.body.rep_file_name)
+  asset.author = req.user.asGitAuthor
 
-  fs.unlink(repFile, function (err) {
-    // Nothing special to do if the file we want to write do not exists
-    // Create the sub-directory if needed
-    mkdirp(path.dirname(repFile), function (err) {
-      if (!err) {
-        // Move the file to its location
-        fs.readFile(tempFile, function (err, data) {
-          if (!err) {
-            fs.writeFile(repFile, data, function (err) {
-              if (!err) {
-                // Remove temporary file
-                fs.unlink(tempFile, function (err) {
-                  // Nothing to do if there is an error
-                  if (err) {
-                    console.warn(err)
-                  }
-                })
-                // Add the written file to git
-                var gitMsg = req.body.file_message
-                if (!gitMsg || gitMsg === '') {
-                  gitMsg = 'Uploading file (' + gitFile + ')'
-                }
-                Git.add(gitFile, gitMsg, req.user.asGitAuthor, function (err) {
-                  if (!err) {
-                    outputMsg = 'File uploaded to: <pre>/uploads/' + repFileName + '</pre>'
-                  } else {
-                    console.warn('Upload: error when commiting file ' + gitFile + ': ' + err)
-                    outputMsg = 'Error when commiting file ' + repFileName + ': <pre>' + err + '</pre>'
-                  }
-                  res.render('upload_status', {message: outputMsg})
-                })
-              } else {
-                console.warn('Upload: error when writing file ' + repFile + ': ' + err)
-                outputMsg = 'Error when writing file ' + repFileName + ' to repository: <pre>' + err + '</pre>'
-                res.render('upload_status', {message: outputMsg})
-              }
-            })
-          } else {
-            console.warn('Upload: error when reading uploaded temporary file' + tempFile + ': ' + err)
-            outputMsg = 'Error when reading uploaded file ' + repFileName + ' : <pre>' + err + '</pre>'
-            res.render('upload_status', {message: outputMsg})
-          }
-        })
-      } else {
-        console.warn('Upload: error when creating path ' + gitFile + ': ' + err)
-        outputMsg = 'Error when creating path ' + repFileName + ': <pre>' + err + '</pre>'
-        res.render('upload_status', {message: outputMsg})
-      }
-    })
+  asset.save(req.file.path, req.body.file_message).then(function () {
+    res.render('upload_status', {message: 'File uploaded to: <pre>' + asset.urlForShow() + '</pre>'})
+  }).catch(function (err) {
+    res.render('upload_status', {message: 'Error: ' + err})
   })
 }
 
